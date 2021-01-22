c222f7b3299f76f5ad2dbac05701fd132e6dcebc
Matskrane <69842790+Matskrane@users.noreply.github.com>
2021-01-22 16:34:38 +0100

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,28 @@
+var toys = [
+	{
+		name: "Lego",
+		price: 15.6,
+	},
+	{
+		name: "Master of the Universe",
+		price: "28.3",
+	},
+	{
+		name: "Barbie",
+		price: null,
+	},
+	{
+		name: "Mr Potato Head",
+		price: 89.99,
+	},
+];
+
+
+function price() {
+    
+}
+
+
+for (var i = 0; i < toys.length; i++) {
+    console.log(toys[i]);
+}
